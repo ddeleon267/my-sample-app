c8751d09dd0e6c7a49305f585f18ce27afec2244
ddeleon267 <dominique.a.deleon@gmail.com>
2020-06-20 14:12:43 -0400

    Add edit form.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,45 @@ class PostsController < ApplicationController
         @posts = Post.all
         erb :'posts/index'
     end
+
+    get '/posts/new' do
+        @users = User.all
+        erb :'/posts/new'
+    end
+
+    post '/posts' do
+        user = User.find_by_id(params[:user_id])
+        post = user.posts.build(params)
+        binding.pry
+        if post.save
+            redirect '/posts'
+        else
+            redirect '/posts/new'
+        end
+    end
+
+    get '/posts/:id' do
+        @post = Post.find_by_id(params[:id])
+        if @post 
+            erb :'/posts/show'
+        else
+            redirect '/posts'
+        end
+    end
+
+    get '/posts/:id/edit' do
+        @post = Post.find_by_id(params[:id])
+        erb :'/posts/edit'
+    end
+    
+    patch '/posts/:id' do
+        post = Post.find_by_id(params[:id])
+        if post.update(title: params[:title], body: params[:body])
+            redirect "/posts/#{post.id}"
+        else
+            redirect "/posts/#{post}/edit"
+        end
+    end
+
+
 end
