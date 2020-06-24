b47c9511204a851a554d8ca77e976ccb8814e0c3
ddeleon267 <dominique.a.deleon@gmail.com>
2020-06-24 11:31:22 -0400

    Add signup form and get/post  action to handle signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
+    get '/signup' do
+        erb :'users/new'
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        if user.save
+            session[:user_id] = user.id
+            redirect '/posts' 
+        else
+            redirect '/signup'
+        end
+    end
 
 end
